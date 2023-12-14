e7dd6cfcd6e55d2335aaaa38e2d3195d9c7ceaea
Michael <michael_lacy95@icloud.com>
2023-12-13 23:20:35 -0600

    solutions

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,49 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here 
-
+// Your code here
+fetch('/products', {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here 
+// Your code here
+fetch('/products', {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+  })
+  .then(res => {
+    console.log(res.status, res.url, res.headers)
+  })
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here 
+// Your code here
+fetch('/products', {
+    method: "POST",
+    body: new URLSearchParams({
+        name: "Caribbean Delight Coffee",
+        description: "Made by Manatee Coffee",
+        price: 11.99,
+        categories: "grocery"
+      }),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+
+.then(res => {
+    console.log(res.status, res.url, res.headers.get('content-type'))
+})
